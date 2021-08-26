050a702bff91fbd6d5a666cbc27b00b902a349c7
camolezi <camolezi@usp.br>
2021-08-25 23:37:09 -0300

    Created test schema and migration

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,13 +1,14 @@
 var ethers = require("ethers");
 var express = require("express");
 var fs = require("fs");
-const https = require('https')
-var cors = require('cors')
+const https = require("https");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
 
-let cache = {}
-let currentMessage = "I am **ADDRESS** and I would like to sign in to YourDapp, plz!"
+let cache = {};
+let currentMessage =
+  "I am **ADDRESS** and I would like to sign in to YourDapp, plz!";
 
 /*
   Uncomment this if you want to create a wallet to send ETH or something...
@@ -21,46 +22,78 @@ const checkWalletBalance = async ()=>{
 checkWalletBalance()
 */
 
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res) {
-    console.log("/")
-    res.status(200).send(currentMessage);
+app.get("/", function (req, res) {
+  console.log("/");
+  res.status(200).send(currentMessage);
 });
 
-app.post('/', async function(request, response){
-    const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-    console.log("POST from ip address:",ip,request.body.message)
-    if(request.body.message!=currentMessage.replace("**ADDRESS**",request.body.address)){
-      response.send(" ⚠️ Secret message mismatch!?! Please reload and try again. Sorry! 😅");
-    }else{
-      let recovered = ethers.utils.verifyMessage(request.body.message, request.body.signature)
-      if(recovered==request.body.address){
-        /*
+app.post("/", async function (request, response) {
+  const ip =
+    request.headers["x-forwarded-for"] || request.connection.remoteAddress;
+  console.log("POST from ip address:", ip, request.body.message);
+  if (
+    request.body.message !=
+    currentMessage.replace("**ADDRESS**", request.body.address)
+  ) {
+    response.send(
+      " ⚠️ Secret message mismatch!?! Please reload and try again. Sorry! 😅"
+    );
+  } else {
+    let recovered = ethers.utils.verifyMessage(
+      request.body.message,
+      request.body.signature
+    );
+    if (recovered == request.body.address) {
+      /*
           maybe you want to send them some tokens or ETH?
         let sendResult = await wallet.sendTransaction({
           to: request.body.address,
           value: ethers.utils.parseEther("0.01")
         })
         */
-        response.send(" 👍 successfully signed in as "+request.body.address+"!");
-      }
+      response.send(
+        " 👍 successfully signed in as " + request.body.address + "!"
+      );
     }
+  }
 });
 
-
-if(fs.existsSync('server.key')&&fs.existsSync('server.cert')){
-  https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-  }, app).listen(49832, () => {
-    console.log('HTTPS Listening: 49832')
-  })
-}else{
+if (fs.existsSync("server.key") && fs.existsSync("server.cert")) {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync("server.key"),
+        cert: fs.readFileSync("server.cert"),
+      },
+      app
+    )
+    .listen(49832, () => {
+      console.log("HTTPS Listening: 49832");
+    });
+} else {
   var server = app.listen(49832, function () {
-      console.log("HTTP Listening on port:", server.address().port);
+    console.log("HTTP Listening on port:", server.address().port);
   });
 }
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function testDatabase() {
+  await prisma.post.create({ data: {} });
+  const allLinks = await prisma.post.findMany();
+  console.log(allLinks);
+}
+
+testDatabase()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
