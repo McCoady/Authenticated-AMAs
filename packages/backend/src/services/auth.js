3b8fefce9c9bb9cf1dee208815085062c343621c
McCoady <78250146+McCoady@users.noreply.github.com>
2021-08-28 14:34:18 +0900

    IERC721 Functionality
    Added IERC721 for abi calls.
    Updated @openzeppelin/contracts module.
    Added variables for verification.
    TO DO: Convert to graphql.

--- packages/backend/src/services/auth.js
+++ packages/backend/src/services/auth.js
@@ -9,6 +9,12 @@ const checkWalletBalance = async ()=>{
 }
 checkWalletBalance()ss
 */
+var ethers = require("ethers");
+
+import IERC721 from ".../hardhat/artifacts/contracts/IERC721.sol/IERC721.json";
+
+const INFURA_ID = env("INFURA_ID");
+const tokenAddress = "0x2414F22e3a423DD63d085dD0d667334F060d733d";
 
 let currentMessage =
   "I am **ADDRESS** and I would like to sign in to YourDapp, plz! s";
@@ -19,37 +25,42 @@ function seedMessage() {
 
 function verifySignedMessage(parent, args, context, info) {
   console.log("mutation", args);
+  //IP needed?
 
-  //Migrate this code to graphQL
 
-  //   const ip =
-  //     request.headers["x-forwarded-for"] || request.connection.remoteAddress;
-  //   console.log("POST from ip address:", ip, request.body.message);
-  //   if (
-  //     request.body.message !=
-  //     currentMessage.replace("**ADDRESS**", request.body.address)
-  //   ) {
-  //     response.send(
-  //       " ⚠️ Secret message mismatch!?! Please reload and try again. Sorry! 😅"
-  //     );
-  //   } else {
-  //     let recovered = ethers.utils.verifyMessage(
-  //       request.body.message,
-  //       request.body.signature
-  //     );
-  //     if (recovered == request.body.address) {
-  //       /*
-  //             maybe you want to send them some tokens or ETH?
-  //           let sendResult = await wallet.sendTransaction({
-  //             to: request.body.address,
-  //             value: ethers.utils.parseEther("0.01")
-  //           })
-  //           */
-  //       response.send(
-  //         " 👍 successfully signed in as " + request.body.address + "!"
-  //       );
-  //     }
-  //   }
+  /* 
+  
+     const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+     console.log("POST from ip address:", ip, request.body.message)
+     if (request.body.message != currentMessage.replace("**ADDRESS**", request.body.address)) {
+         response.send(" ⚠️ Secret message mismatch!?! Please reload and try again. Sorry! 😅");
+     } else {
+         let recovered = ethers.utils.verifyMessage(request.body.message, request.body.signature)
+ 
+         const userAddress = request.body.address;
+ 
+         if (recovered == request.body.address) {
+ 
+             const ropstenInfura = new ethers.providers.StaticJsonRpcProvider("https://mainnet.infura.io/v3/" + INFURA_ID);
+ 
+             const tokenContract = new ethers.Contract(tokenAddress, IERC721.abi, ropstenInfura)
+ 
+             const tokenBalance = await tokenContract.balanceOf(userAddress);
+ 
+             const userHasTokens = tokenBalance && tokenBalance.gt(0);
+ 
+             if (userHasTokens) {
+                 response.send(" 👍 successfully signed in as [" + userAddress + "]! Ask away!");
+                 //**Access hidden content**
+             } else {
+                 response.send(" 🤔 successfully signed in as [" + userAddress + "]... But you don't have the required token to participate.");
+ 
+             }
+ 
+ 
+         }
+     }
+ });*/
 }
 
 const AuthResolver = {
